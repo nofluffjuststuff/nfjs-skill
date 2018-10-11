8edb2da34f08e96e5c350c5e9fd62776f74c2e49
Craig Walls <craig@habuma.com>
2018-10-10 22:32:38 -0600

    Add .ask folder omitted from previous commit; Say goodbye a little more friendly.

--- lambda/custom/StandardHandlers.js
+++ lambda/custom/StandardHandlers.js
@@ -59,7 +59,9 @@ const CancelAndStopIntentHandler = {
         || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
   },
   handle(handlerInput) {
-    const speechText = 'Thanks for asking about NFJS! Goodbye!';
+    const speechText = `
+    <speak>Thanks for asking about NFJS! <say-as interpret-as="interjection">Goodbye</say-as>!</speak>
+    `;
 
     return handlerInput.responseBuilder
       .speak(speechText)
